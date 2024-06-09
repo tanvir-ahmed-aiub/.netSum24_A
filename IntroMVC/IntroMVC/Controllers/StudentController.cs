b61df52a0807a20d7c0531c779a4886e59b805f9
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2024-06-09 15:13:23 +0600

    Form Submission

---
+++ IntroMVC/IntroMVC/Controllers/StudentController.cs
@@ -0,0 +1,39 @@
+﻿using IntroMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntroMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Student s)
+        {
+            //fc["Name"] 
+            //fc["Email]
+            //fc["Gender"]
+            return View();
+        }
+        //[HttpPost]
+        //public ActionResult Create(string Name, string Email)
+        //{
+        //    return View();
+        //}
+        //[HttpPost]
+        //public ActionResult Create(FormCollection fc) {
+        //    //fc["Name"] 
+        //    //fc["Email]
+        //    //fc["Gender"]
+        //    return View();
+        //}
+    }
+}
--- IntroMVC/IntroMVC/Models/Student.cs
+++ IntroMVC/IntroMVC/Models/Student.cs
@@ -8,6 +8,10 @@ namespace IntroMVC.Models
     public class Student
     {
         public string Name { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public string Profession { get; set; }
+
         public int Id { get; set; }
         public float Cgpa { get; set; }
 
